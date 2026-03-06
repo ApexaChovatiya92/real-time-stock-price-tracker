55287ea834f194a016086f669445be2a0d78e3f0
Milan Pambhar <mdpambhar@gmail.com>
2026-03-06 21:37:39 +0530

    Initial Commit

---
+++ Real-Time Price Tracker App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Real-Time Price Tracker App
+//
+//  Created by apexa Chovatiya on 06/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Real-Time Price Tracker App/Real_Time_Price_Tracker_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Time_Price_Tracker_AppApp.swift
+//  Real-Time Price Tracker App
+//
+//  Created by apexa Chovatiya on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Real_Time_Price_Tracker_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
